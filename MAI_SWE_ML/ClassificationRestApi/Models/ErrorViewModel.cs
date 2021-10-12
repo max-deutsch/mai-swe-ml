ee856c9dae95baa625dc02fe4ff790268d7cf2b0
Max Deutsch <max.deutsch@posteo.at>
2021-10-12 14:09:37 +0200

    generate api projects

---
+++ MAI_SWE_ML/ClassificationRestApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClassificationRestApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
